b2dc8f4d63dae3c7192ead729d6ab9217d7f7c13
Yobu <121264075+Ur-dad24@users.noreply.github.com>
2024-09-20 23:01:13 +0530

    Initial frontend setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import Header from "./components/Header.jsx";
+import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+import Footer from "./components/Footer.jsx";
 
 const App = () => {
-
   return (
-    <h1>Welcome to ProShop</h1>
-  )
-}
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
+};
 
-export default App
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import App from "./App.jsx";
+import "./assets/styles/bootstrap.custom.css";
+import "./assets/styles/index.css";
+import HomeScreen from "./screens/HomeScreen.jsx";
+import ProductScreen from "./screens/ProductScreen.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="product/:id" element={<ProductScreen />} />
+    </Route>
+  )
+);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
