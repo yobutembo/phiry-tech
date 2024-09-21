30a7d7657ebbf6268a5b06d10e8b3e856efcb6b2
Yobu <121264075+Ur-dad24@users.noreply.github.com>
2024-09-21 18:50:37 +0530

    Connect to MongoDB using mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-dotenv.config();
 import products from "./data/products.js";
+import connectDB from "./config/db.js";
+dotenv.config();
 const port = process.env.PORT || 5000;
+connectDB(); //Connect to MongoDB
 const app = express();
 
 app.use(cors());
