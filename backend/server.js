dce20166ae0e5afd6a2d761fc3f922bfc2729a5a
Yobu <121264075+yobutembo@users.noreply.github.com>
2024-09-24 23:59:29 +0530

    Get Products from database

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import products from "./data/products.js";
+// import products from "./data/products.js";
+import Product from "./models/productModel.js";
 import connectDB from "./config/db.js";
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -14,12 +15,19 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
+app.get("/api/products", async (req, res) => {
+  try {
+    const response = await Product.find({});
+    const products = response;
+    res.json(products);
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    res.status(500).json({ message: "Server Error" });
+  }
 });
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
+app.get("/api/products/:id", async (req, res) => {
+  const product = await Product.findById(req.params.id);
   res.json(product);
 });
 
