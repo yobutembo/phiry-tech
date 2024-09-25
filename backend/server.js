608e7818e2514acd21bc923e454908a2d947ed46
Yobu <121264075+yobutembo@users.noreply.github.com>
2024-09-25 13:53:13 +0530

    feat: Implement express router for product routes

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-// import products from "./data/products.js";
-import Product from "./models/productModel.js";
 import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
 dotenv.config();
 const port = process.env.PORT || 5000;
 connectDB(); //Connect to MongoDB
@@ -15,21 +14,7 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.get("/api/products", async (req, res) => {
-  try {
-    const response = await Product.find({});
-    const products = response;
-    res.json(products);
-  } catch (error) {
-    console.log(`Error: ${error.message}`);
-    res.status(500).json({ message: "Server Error" });
-  }
-});
-
-app.get("/api/products/:id", async (req, res) => {
-  const product = await Product.findById(req.params.id);
-  res.json(product);
-});
+app.use("/api/products", productRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
