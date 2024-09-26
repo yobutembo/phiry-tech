50286cfef3204c0aeab4b8e71916b9bdaa18ea0f
Yobu <121264075+yobutembo@users.noreply.github.com>
2024-09-26 18:49:31 +0530

    Setup Redux Store and API Slice

---
+++ frontend/src/slices/apiSlice.js
@@ -0,0 +1,13 @@
+//This is going to be the parent slice to our other api slices
+//We are going to use the createApi function from the redux toolkit to create this slice
+
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { BASE_URL } from "../constants";
+
+//This is the base query that is going to be used by the createApi function
+const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
+export const apiSlice = createApi({
+  baseQuery,
+  tagTypes: ["Product", "Order", "user"], //tagTypes are used to group together actions that are related to the same entity
+  endpoints: (builder) => ({}),
+});
