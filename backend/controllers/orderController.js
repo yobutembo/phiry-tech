3f44dad7eb3b54b9f7b245d53623543c2476d147
Yobu <121264075+yobutembo@users.noreply.github.com>
2024-10-15 17:43:48 +0530

    Setup all the controller functions and routes for orders

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,12 +1,59 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import Order from "../models/orderModel.js";
 
-//@desc
-//@route
-//@access
+//@desc Create new order
+//@route POST /api/orders
+//@access Private
 
-const getOrders = asyncHandler(async (req, res) => {
-  res.json("get orders");
+const addOrderItems = asyncHandler(async (req, res) => {
+  res.json("add order items");
 });
 
-export { getOrders };
+//@desc Get logged in user's orders
+//@route GET /api/orders/myorders
+//@access Private
+
+const getMyOrders = asyncHandler(async (req, res) => {
+  res.json("get my orders ");
+});
+
+//@desc Get order by
+//@route GET /api/orders/:id
+//@access Private
+
+const getOrderById = asyncHandler(async (req, res) => {
+  res.json("get order by id ");
+});
+
+//@desc Update order to paid
+//@route GET /api/orders/:id/pay
+//@access Private/Admin
+
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  res.json("update order to paid ");
+});
+
+//@desc Update order to delivered
+//@route GET /api/orders/:id/deliver
+//@access Private/Admin
+
+const updateOrderToDelivered = asyncHandler(async (req, res) => {
+  res.json("update order to delivered ");
+});
+
+//@desc Get all orders
+//@route GET /api/orders
+//@access Private/Admin
+
+const getOrders = asyncHandler(async (req, res) => {
+  res.json("get all orders");
+});
+
+export {
+  addOrderItems,
+  getOrders,
+  getMyOrders,
+  getOrderById,
+  updateOrderToPaid,
+  updateOrderToDelivered,
+};
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,22 @@
 import express from "express";
 
-import { getOrders } from "../controllers/orderController.js";
+import {
+  addOrderItems,
+  getOrders,
+  getMyOrders,
+  getOrderById,
+  updateOrderToPaid,
+  updateOrderToDelivered,
+} from "../controllers/orderController.js";
+
+import { protect, admin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.get("/", getOrders);
+router.route("/").post(protect, addOrderItems).get(protect, admin, getOrders);
+router.route("/mine").get(protect, getMyOrders);
+router.route("/:id").get(protect, admin, getOrderById);
+router.route("/:id/pay").put(protect, admin, updateOrderToPaid);
+router.route("/:id/deliver").put(protect, admin, updateOrderToDelivered);
 
 export default router;
