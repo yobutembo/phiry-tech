8d6b69da11ed2dfd99c10958d1401bed5076f897
Yobu <121264075+yobutembo@users.noreply.github.com>
2024-11-02 01:03:46 +0530

    Update UserListScreen

--- frontend/src/screens/admin/UserListScreen.jsx
+++ frontend/src/screens/admin/UserListScreen.jsx
@@ -1,7 +1,74 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import { Table, Button } from "react-bootstrap";
+import { FaTrash, FaTimes, FaEdit, FaCheck, FaEye } from "react-icons/fa";
+import Message from "../../components/Message";
+import Loader from "../../components/Loader";
+import { useGetUsersQuery } from "../../slices/usersApiSlice";
 
 const UserListScreen = () => {
-  return <div>UserListScreen</div>;
-};
+  const { data: users, refetch, isLoading, error } = useGetUsersQuery();
 
+  const deleteHandler = (id) => {
+    console.log("delete");
+  };
+  return (
+    <>
+      <h1>Users</h1>
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Table striped hover responsive className="table-sm">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>NAME</th>
+              <th>EMAIL</th>
+              <th>ADMIN</th>
+
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user._id}>
+                <td>{user._id}</td>
+                <td>{user.name}</td>
+                <td>
+                  <a href={`mailto:${user.email}`}>{user.email}</a>
+                </td>
+                <td>
+                  {user.isAdmin ? (
+                    <FaCheck style={{ color: "green" }} />
+                  ) : (
+                    <FaTimes style={{ color: "red" }} />
+                  )}
+                </td>
+
+                <td>
+                  <Button
+                    as={Link}
+                    to={`/admin/user/${user._id}/edit`}
+                    variant="light"
+                    className="btn-sm"
+                  >
+                    <FaEdit />
+                  </Button>
+                  <Button
+                    variant="light"
+                    className="btn-sm"
+                    onClick={() => deleteHandler(user._id)}
+                  >
+                    <FaTrash style={{ color: "red" }} />
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
+    </>
+  );
+};
 export default UserListScreen;
--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -34,6 +34,7 @@ export const usersApiSlice = apiSlice.injectEndpoints({
       query: () => ({
         url: `${USERS_URL}`,
       }),
+      providesTags: ["Users"],
       keepUnusedDataFor: 5,
     }),
   }),
@@ -44,4 +45,5 @@ export const {
   useLogoutMutation,
   useRegisterMutation,
   useProfileMutation,
+  useGetUsersQuery,
 } = usersApiSlice;
